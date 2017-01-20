ed77e287e96b0b4884765a6dcdb208fb6a055150
Evan Gray <evanlawgray@gmail.com>
2017-01-19 23:35:03 -0800

    Implement validate email function.

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,25 @@
 $(function(){
 
+	var emailSubmitButton = $('#email-submit-button');
+
+	function validateEmail(email){
+			var filter = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            if (filter.test(email)) {
+            	alert("Thanks for signing up!");
+            } else {
+            	alert("You didn't enter a valid e-mail. Please try again");
+            }
+		};
+
+	/*$('email-input').on('focus', function hideText() {
+		$('email-input').text("");
+	});*/
+
+	emailSubmitButton.on('click', function(event){
+		event.preventDefault();
+		var emailSubmitted = $('.email-input').val();
+		console.log(emailSubmitted);
+		validateEmail(emailSubmitted);
+	});
+
 });
