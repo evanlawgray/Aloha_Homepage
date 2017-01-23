124c1289e5c371b672f4628e7ce1573caa91d47e
Evan Gray <evanlawgray@gmail.com>
2017-01-22 23:09:36 -0800

    Adjust about padding.

--- scripts/main.js
+++ scripts/main.js
@@ -30,7 +30,7 @@ $(function(){
 		resize: false,
 		setGallerySize: false,
 		initialIndex: 0,
-		cellSelector: '.product-list-item',
+		//cellSelector: '.product-list-item',
 		cellAlign: 'left',
 		groupCells: false,
 		percentPosition: true,
